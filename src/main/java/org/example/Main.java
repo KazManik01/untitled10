47275fe86381026676af50e65bc75dd06b3cb155
Rybin <asd2@DAS>
2023-04-07 19:03:39 +1200

    Код

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,15 @@
 package org.example;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
 import java.util.*;
-import org.apache.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -23,31 +20,30 @@ public class Main {
         System.out.println("720p - 0, 1080p - 1,360p - 2,480p - 3,");
         Scanner scanner = new Scanner(System.in);
         int choose = scanner.nextInt();
-        String jj = "";
-        while (links.size() + 1 > schet) {
-            String link = "AdaptationSet:eq(0) > Representation:eq(" + choose + ") > SegmentList > SegmentURL:eq(" + i + ")";
-            jj = ((doc.select(link).attr("media")));
+        String jj = "!";
+        while (!jj.isEmpty()) {
+            String link = "Representation[height=720] > SegmentList > SegmentURL:eq(" + i + ")";
+            jj = new String((doc.select(link).attr("media")));
             links.add(jj);
             i++;
             schet++;
-
-            if (jj.isEmpty()) {
-                System.out.println("String is empty");
-                break;
-            }
         }
+
         Set<String> linkSet = new HashSet<>(links);
         links.clear();
         links.addAll(linkSet);
-        System.out.println(links);
+        links.forEach(System.out::println);
         download(links);
 
 
     }
 
     public static void download(ArrayList<String> links) throws IOException {
-        for (int i = 0; i < links.size(); i++) {
-            FileUtils.copyURLToFile(new URL(links.get(i)), new File("C:/Users/Rybin/Videos"));
+        for (int i = 1; i < links.size(); i++) {
+            FileUtils.copyURLToFile(new URL(links.get(i)), new File("C:\\Новая папка\\test "+ i +".mp4"));
+            if (links.get(i).isEmpty()){
+                break;
+            }
 
 
         }
